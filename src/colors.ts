62aaa6ffd09a9465d9cc7029be844a275b8354f7
ENvironmentSet <sbshw1@gmail.com>
2024-08-04 17:51:10 +0900

    Add background/foreground color setting

---
+++ src/colors.ts
@@ -0,0 +1,7 @@
+export const enum Color {
+  background = '#19191B',
+  foreground = '#B3B2B6',
+  primaryText = '#E6E6EA',
+  secondaryText = '#595B62',
+  highlight = '#ED5C07'
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,24 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
+import { Global, css } from '@emotion/react'
+
 import './reset.css'
 import './font.css'
+import { Color } from './colors.ts';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Global
+      styles={css`
+        body {
+          font-family: GmarketSans;
+          
+          color: ${Color.foreground};
+          background-color: ${Color.background};
+        }
+      `}
+    />
     <App />
   </StrictMode>,
 )
