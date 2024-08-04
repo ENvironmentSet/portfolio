ef6b355acde24292764802d8a0c70cf9e23a3c37
ENvironmentSet <sbshw1@gmail.com>
2024-08-04 17:55:50 +0900

    rearrange imports

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
 import { Global, css } from '@emotion/react'
+import { Color } from './colors.ts'
 
 import './reset.css'
 import './font.css'
-import { Color } from './colors.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
