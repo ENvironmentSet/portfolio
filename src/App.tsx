6387fdaf2d4eb405cce4745edd1f94ad5e2f16f0
ENvironmentSet <sbshw1@gmail.com>
2024-08-05 13:25:58 +0900

    move global styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,40 @@
+import { Switch, Route, Redirect } from 'wouter'
+import Index from './Index'
+
+import cardEntries from './contents/card-entries.ts'
+
+import { css, Global } from '@emotion/react'
+import { Color } from './colors.ts'
+
+import './reset.css'
+import './font.css'
+
 function App() {
-  return <h1> Hello, world! </h1>
+  return (
+    <>
+      <Global
+        styles={css`
+        body {
+          font-family: GmarketSans;
+          
+          color: ${Color.foreground};
+          background-color: ${Color.background};
+        }
+        
+        @media (max-width: 640px) {
+          html {
+            font-size: 12px;
+          }
+        }
+      `}
+      />
+      <Switch>
+        <Route path='/'> <Index cardEntries={cardEntries} /> </Route>
+
+        <Route> <Redirect to='/' /> </Route>
+      </Switch>
+    </>
+  )
 }
 
 export default App
--- src/main.tsx
+++ src/main.tsx
@@ -2,24 +2,8 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 
-import { Global, css } from '@emotion/react'
-import { Color } from './colors.ts'
-
-import './reset.css'
-import './font.css'
-
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Global
-      styles={css`
-        body {
-          font-family: GmarketSans;
-          
-          color: ${Color.foreground};
-          background-color: ${Color.background};
-        }
-      `}
-    />
     <App />
   </StrictMode>,
 )
