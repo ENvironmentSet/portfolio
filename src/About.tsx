baf14a501b224d40b824514316795c9dba612346
ENvironmentSet <sbshw1@gmail.com>
2024-08-16 15:05:10 +0900

    make about page responsive

--- src/About.tsx
+++ src/About.tsx
@@ -1,6 +1,10 @@
 import { css } from '@emotion/react'
+import { Link } from 'wouter'
 
-import { IoIosArrowBack } from 'react-icons/io'
+import { IoIosArrowBack, IoMdInformationCircle } from 'react-icons/io'
+import { IoDocumentSharp } from 'react-icons/io5'
+import { FaHeart } from 'react-icons/fa'
+
 import Color from './colors.ts'
 
 const AboutStyle = {
@@ -13,9 +17,17 @@ const AboutStyle = {
     
     border-bottom: 1px solid ${Color.secondaryText};
     
-    position: relative;
-    
     font-size: 1.4rem;
+    
+    @media (max-width: 1023px) {
+      position: relative;
+    }
+    
+    @media (min-width: 1024px) {
+      margin: 0 15%;
+      
+      font-size: 1.8rem;
+    }
   `,
   backButton: css`
     position: absolute;
@@ -23,22 +35,99 @@ const AboutStyle = {
     
     height: 1.4rem;
     width: 1.4rem;
+    
+    @media (min-width: 1024px) {
+      display: none;
+    }
   `,
   main: css`
-    padding: 2rem;
+    padding: 2rem 20%;
   `,
   highlight: css`
     color: ${Color.highlight};
     font-weight: bold;
-  `
+  `,
+  navBar: css`
+    position: absolute;
+    top: 5.5rem;
+    left: 0;
+      
+    padding: 1.5rem 1.5rem;
+    
+    font-size: 1rem;
+      
+    height: auto;
+      
+    border: none;
+    border-right: 2px solid ${Color.secondaryText};
+  `,
+  navList: css`
+    height: 100%;
+  
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1.5rem;
+    
+    list-style-type: none;
+    
+    padding-left: 0px;
+  `,
+  navItem: css`
+    flex-grow: 1;
+    
+    button {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      align-items: center;
+      
+      background-color: transparent;
+      
+      border: 0px;
+      padding: 0px;
+      
+      width: 100%;
+      
+      color: ${Color.primaryText};
+    }
+  `,
 }
 
 function About() {
   return (
     <>
       <header css={AboutStyle.header}>
-        <IoIosArrowBack onClick={() => history.back()} css={AboutStyle.backButton} />
+        <IoIosArrowBack onClick={() => history.back()} css={AboutStyle.backButton}/>
         <b>곶감에 대해</b>
+        <nav css={AboutStyle.navBar}>
+          <ul css={AboutStyle.navList}>
+            <li css={AboutStyle.navItem}>
+              <Link to='/' asChild>
+                <button>
+                  <IoDocumentSharp/>
+                  <span>포트폴리오</span>
+                </button>
+              </Link>
+            </li>
+            <li css={AboutStyle.navItem}>
+              <Link to='/likes' asChild>
+                <button>
+                  <FaHeart/>
+                  <span>찜 목록</span>
+                </button>
+              </Link>
+            </li>
+            <li css={AboutStyle.navItem}>
+              <Link to='/about' asChild>
+                <button>
+                  <IoMdInformationCircle/>
+                  <span>곶감에 대해</span>
+                </button>
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
       <main css={AboutStyle.main}>
         <h1>제가 곧 갑니다 : <span css={AboutStyle.highlight}>곶감</span></h1>
